078ffc71c8a9011d9dc94b4badc130418d08930d
yasha6081 <yasha6081@gmail.con>
2021-03-09 11:32:33 -0600

    listen

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.set('views', 'views');
 app.set('view engine', 'ejs')
 
@@ -10,4 +15,9 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(3000);
+// Starts the server to begin listening
+// =============================================================
+
+app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
