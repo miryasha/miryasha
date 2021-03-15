345d66756bc24b6f91f762aa88694cb77786abaa
yasha6081 <yasha6081@gmail.con>
2021-03-15 18:02:56 -0500

    login added

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-const express = require('express');
-const app = express();
+const express = require("express");
 
+const controllers = require("./app/controllers");
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.set('views', 'views');
-app.set('view engine', 'ejs')
+function passwordProtected(req, res, next){
+  res.set('WWW-Authenticate','Basic realm="Dash Board"')
+  //console.log(req.headers.authorization)
+  
+  if (req.headers.authorization == "Basic SkVUWTQ1NzYzJCMmKiMlWUhodmZKREZTRmRndnNkZnNHYTMzNTR5NjQ1Oj8iTExLUEZQR1NkZmdwbzQ5cTh3ZXBxazc4XiUkJCMjZmd1Jmlsbm1LTEs=") {
+    next()
 
+  }else{
+    res.status(401).send("Authentication required")
+  }
+}
+
+app.use(passwordProtected)
+
+// Static directory to be served
+app.use(express.static("./app/public"));
+
+
+
+// Routes
+// =============================================================
+app.use("/", controllers.landingPage);
+app.use("/albums", controllers.albums);
+app.use("/certificates", controllers.certificates);
+app.use("/login", controllers.login);
 
 
 
